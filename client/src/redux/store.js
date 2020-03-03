3c9b749c8561b681dab20edefce333789ba99439
bradypp <bradypp44@gmail.com>
2020-03-03 20:36:49 +0000

    Added redux dev tools

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,14 +1,16 @@
 import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import { persistStore } from 'redux-persist';
+import reduxThunk from 'redux-thunk';
 import logger from 'redux-logger';
 import rootReducer from './rootReducer';
 
-const middlewares = [];
+const middlewares = [reduxThunk];
 
 if (process.env.NODE_ENV === 'development') {
     middlewares.push(logger);
 }
 
-export const store = createStore(rootReducer, applyMiddleware(...middlewares));
+export const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(...middlewares)));
 
 export const persistor = persistStore(store);
