10299bcec16131a722cb91175be343b0dd0c2843
bradypp <bradypp44@gmail.com>
2020-03-03 20:29:29 +0000

    Added custom bootstrap theme

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,11 @@ import React from 'react';
 import 'styles/main.scss';
 
 function App() {
-    return <div className="App"></div>;
+    return (
+        <div className="App">
+            <h1>My React App</h1>
+        </div>
+    );
 }
 
 export default App;
