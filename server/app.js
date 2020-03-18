60a68a30a23a94e2189ab89bab71707979d043de
bradypp <bradypp44@gmail.com>
2020-03-18 16:12:39 +0000

    Added extra security middlewares

--- server/app.js
+++ server/app.js
@@ -3,6 +3,9 @@ const cors = require('cors');
 const morgan = require('morgan');
 const cookieParser = require('cookie-parser');
 const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
 const AppError = require('./utils/appError');
 const rateLimiter = require('./utils/rateLimiter');
 const globalErrorHandler = require('./controllers/handlers/globalErrorHandler');
@@ -21,7 +24,7 @@ app.use(helmet());
 app.use(cors());
 app.options('*', cors());
 
-// Logger Middleware
+// Logger middleware
 if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'));
 }
@@ -36,10 +39,29 @@ if (process.env.NODE_ENV === 'production') {
     app.use('/api/v1/auth/update-password', rateLimiter());
 }
 
-// Body-parsing Middleware
-app.use(express.json({ extended: false }));
+// Body-parsing middlewares
+app.use(express.json({ limit: '10kb' }));
+app.use(express.urlencoded());
 
-// Cookie-parsing Middleware
+// Data sanitization against NoSQL query injection & XSS
+app.use(mongoSanitize());
+app.use(xss());
+
+// Prevent parameter pollution
+app.use(
+    hpp({
+        whitelist: [
+            'duration',
+            'ratingsQuantity',
+            'ratingsAverage',
+            'maxGroupSize',
+            'difficulty',
+            'price',
+        ],
+    }),
+);
+
+// Cookie-parsing middleware
 app.use(cookieParser());
 
 // Routes
