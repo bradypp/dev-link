ef35a1b76def9fbee564d5b5f8ad3590eae104db
bradypp <bradypp44@gmail.com>
2020-04-08 20:41:20 +0100

    Added box shadow mixins tothe theme

--- client/src/App/ThemeProvider/Theme/utils.js
+++ client/src/App/ThemeProvider/Theme/utils.js
@@ -1,4 +1,5 @@
 const utils = {
+    rootDomain: {},
     zIndex: {
         dropdown: 50,
         modal: 100,
@@ -8,6 +9,12 @@ const utils = {
     layout: {
         maxWidth: `120rem`,
         pagePadding: `4rem`,
+    },
+    boxShadow: {
+        medium: `0 0.5rem 1rem 0 rgba(0, 0, 0, 0.1)`,
+        card: `0 0 0 0.1rem rgba(0, 0, 0, 0.15), 0 0.2rem 0.5rem rgba(0, 0, 0, 0.2)`,
+        dropdown: `rgba(9, 30, 66, 0.25) 0 0.4rem 0.8rem -0.2rem, rgba(9, 30, 66, 0.31) 0 0x 0.1rem`,
+        cover: `0 0 1rem rgba(0, 0, 0, 0.3)`,
     },
 };
 
