5bad0d8a7bd01ed9f9e1db3a8bac0ea4245d6033
bradypp <bradypp44@gmail.com>
2020-05-16 11:26:56 +0100

    Updated client static files path

--- app.js
+++ app.js
@@ -77,9 +77,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Serve client in production
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/build')));
+    app.use(express.static(path.join(__dirname, 'client/build')));
     app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, '../client/build/index.html'));
+        res.sendFile(path.join(__dirname, 'client/build/index.html'));
     });
 }
 
