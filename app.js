158c5a556e8583336111b0e81aa664c5b8d0110d
bradypp <bradypp44@gmail.com>
2020-05-16 11:54:25 +0100

    Updated cors

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use(helmet());
 // Implement CORS
 app.use(
     cors({
-        origin: 'https://boiling-atoll-38153.herokuapp.com',
+        origin: 'http://localhost:3000',
         optionsSuccessStatus: 200,
         credentials: true,
     }),
