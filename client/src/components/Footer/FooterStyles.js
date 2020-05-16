9a78f2c7475c1ca7b751e1636e4534def6d16d5d
bradypp <bradypp44@gmail.com>
2020-05-16 10:50:56 +0100

    Updated foter styles

--- client/src/components/Footer/FooterStyles.js
+++ client/src/components/Footer/FooterStyles.js
@@ -6,13 +6,14 @@ export const FooterContainer = styled.footer`
     flex-wrap: wrap;
     padding: 2rem ${({ theme }) => theme.layout.pagePadding} 0;
     height: ${({ theme }) => theme.layout.footerHeight};
+    background-color: ${({ theme }) => theme.colors.background2};
 
     ${media.bp800`
         padding: 2rem ${({ theme }) => theme.layout.pagePaddingTablet} 0;
     `}
     ${media.bp440`
         padding: 2rem ${({ theme }) => theme.layout.pagePaddingMobile} 0;
-    `}
+    `};
 `;
 
 export const Separator = styled.span`
